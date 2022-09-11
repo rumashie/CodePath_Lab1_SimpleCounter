bb66fa0591cbd2369aa921fa2f10caaf87fbbc28
yomashie <massielrsanchez@gmail.com>
2022-09-11 02:10:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/codepathlab1simplecounter/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.codepathlab1simplecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val upgradeButton = findViewById<Button>(R.id.upgradeButton)
+
+        button.setOnClickListener {
+            Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            button.setOnClickListener {
+                // Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+                counter++
+                textView.text = counter.toString()
+
+                if (counter >= 100) {
+
+                    // Show upgrade button and set onClickListener
+                    upgradeButton.visibility = View.VISIBLE
+                    upgradeButton.setOnClickListener {
+                        button.text = "Add 2"
+
+                        // Update original button to add 2 instead of `
+                        button.setOnClickListener {
+                            counter += 2
+                            textView.text = counter.toString()
+                        }
+
+                        // Hide upgrade button again
+                        upgradeButton.visibility = View.INVISIBLE
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
